e297bb8f5256bf2f52b6b9d0d2140f25c7f2715d
Aura Mendez <auralecomte@gmail.com>
2023-05-04 00:44:17 +0200

    Adjust columns in Agenda an make past concerts not always visible

--- src/components/SectionAgenda.vue
+++ src/components/SectionAgenda.vue
@@ -1,21 +1,21 @@
 
 <template>
     <div class="section-agenda">
-        <h2>Tour Dates</h2>
+        <h1 class="my-6">Tour Dates</h1>
 
         <v-container class="agenda-list">
             <v-row v-for="concert in agendaStore.futureConcerts" :key="concert.id">
-                <v-col cols="3">
+                <v-col cols="3" class="date">
                     <p>{{ formatDate(concert.date) }}</p>
                 </v-col>
-                <v-col cols="3">
+                <v-col cols="4" class="program">
                     <p>{{ concert.program }}</p>
                 </v-col>
-                <v-col cols="3">
+                <v-col cols="3" class="venue">
                     <p>{{ concert.location }}</p>
                     <p>{{ concert.venue }}</p>
                 </v-col>
-                <v-col cols="3">
+                <v-col cols="2" class="tickets">
                     <p v-if="concert.soldOut">Sold out</p>
                     <p v-else-if="concert.freeEntrance">Free entrance</p>
                     <v-btn v-else-if="concert.ticketsUrl" :href="concert.ticketsUrl" target="_blank">Tickets</v-btn>
@@ -23,17 +23,21 @@
             </v-row>
         </v-container>
 
-        <h4>Past concerts</h4>
+        <div class="past-concerts-header my-6" @click="showPastConcerts = !showPastConcerts">
+            <h4>Past concerts</h4>
+            <v-icon v-if="showPastConcerts" icon="mdi-chevron-up"></v-icon>
+            <v-icon v-else icon="mdi-chevron-down"></v-icon>
+        </div>
 
-        <v-container class="agenda-list">
+        <v-container class="agenda-list" v-if="showPastConcerts">
             <v-row v-for="concert in agendaStore.pastConcerts" :key="concert.id">
-                <v-col cols="3">
+                <v-col cols="3" class="date">
                     <p>{{ formatDate(concert.date) }}</p>
                 </v-col>
-                <v-col cols="4">
+                <v-col cols="4" class="program">
                     <p>{{ concert.program }}</p>
                 </v-col>
-                <v-col cols="3">
+                <v-col cols="2" class="venue">
                     <p>{{ concert.venue }}, {{ concert.location }}</p>
                 </v-col>
             </v-row>
@@ -44,18 +48,20 @@
 
 <script>
 import { useAgendaStore } from "@/stores/agenda"
+import { ref } from "vue";
 
 export default {
     setup() {
 
         const agendaStore = useAgendaStore();
 
+        const showPastConcerts = ref(false);
         function formatDate(dateStr) {
             const formattedDate = new Date(dateStr).toDateString().slice(4);
             return formattedDate;
         }
 
-        return { agendaStore, formatDate };
+        return { agendaStore, formatDate, showPastConcerts };
     },
 }
 </script>
@@ -63,8 +69,22 @@ export default {
 <style scoped>
 
 .section-agenda {
-    text-align: center;
     padding-bottom: 6rem;
+}
+
+.tickets {
+    text-align: end;
+}
+
+.past-concerts-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+}
+.past-concerts-header:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+    cursor: pointer;
 }
 
 /* Desktop */
