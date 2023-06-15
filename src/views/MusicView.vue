14db73a9780726b891729cba3c69aba0223c94ee
Aura Mendez <auralecomte@gmail.com>
2023-06-15 23:15:57 +0200

    Restyle Music view

--- src/views/MusicView.vue
+++ src/views/MusicView.vue
@@ -3,22 +3,28 @@
         <div class="music-section text-center">
             <img class="title mb-8" src="../../public/title-music-min.png" alt="" height="30">
 
-            <v-list>
-                <v-list-item v-for="(doc, i) in musicStore.personalMusic" :key="i">
-                    {{ doc.img }}
-                </v-list-item>
-            </v-list>
+            <template v-for="(item, i) in musicStore.personalMusic" :key="i">
+                <MusicItem :item="item"></MusicItem>
+            </template>
+            
+            <img class="title mb-8" src="../../public/separator-1-min.png" alt="" height="30">
+            
+            <template v-for="(item, i) in musicStore.sidemanMusic" :key="i">
+                <MusicItem :item="item"></MusicItem>
+            </template>
         </div>
     </DefaultLayout>
 </template>
 
 <script>
 import DefaultLayout from '../layouts/DefaultLayout.vue';
+import MusicItem from '../components/MusicItem.vue';
 import { useMusicStore } from '../stores/music';
 
 export default {
     components: {
         DefaultLayout,
+        MusicItem,
     },
     setup() {
         const musicStore = useMusicStore();
