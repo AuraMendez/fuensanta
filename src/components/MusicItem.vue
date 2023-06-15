c7ce5aad96b4b1c0665c66c116335551c7a51900
Aura Mendez <auralecomte@gmail.com>
2023-06-15 23:17:19 +0200

    Belongs to previous commit

---
+++ src/components/MusicItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <a :href="item.url" target="_blank">
+            <img :src="item.img.url">
+        </a>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        item: Object,
+    },
+}
+</script>
+
+<style scoped>
+div {
+    margin: 3rem 0;
+    text-align: center;
+}
+
+a img {
+    width: 100%;
+    max-width: 600px;
+}
+</style>
